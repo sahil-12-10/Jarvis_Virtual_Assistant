a46bcc15ffb379f556efb8ef0188d85552389d38
Sahil Sayed <109230620+sahil-12-10@users.noreply.github.com>
2024-07-26 01:34:05 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "tauba" : "https://www.youtube.com/watch?v=LK7-_dgAVQE",
+    "bebebaapu":"https://www.youtube.com/watch?v=cpKolP6mMec",
+    "ruaan":"https://www.youtube.com/watch?v=OPazrdwYAm0"
+}
